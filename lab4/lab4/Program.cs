7eda348ecf5798ea83640f4b26442393b695452e
Yana <68700729+HanyHanya@users.noreply.github.com>
2020-10-26 22:41:50 +0300

    1

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lab4
+{
+
+    class Stack
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>();
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+            numbers.Push(4);
+            numbers.Push(5);
+            numbers.Push(6);
+            while (!numbers.IsEmpty)
+            {
+                int numb = numbers.Pop();
+                Console.WriteLine(numb);
+            }
+        }
+    }
+}
+
+
---
+++ lab4/lab4/Stack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab4
+{
+    public class Stack<S>
+    {
+        private S[] items;
+        private int count;//количество элементов в стеке
+        const int n = 10;//максимальное количество элементов
+        public Stack()
+        {
+            items = new S[n];
+        }
+
+        public bool IsEmpty//пуст или нет
+        {
+            get { return count == 0; }
+        }
+        public int Count//количество элементов
+        {
+            get { return count; }
+        }
+
+        public void Push(S item)//добавление
+        {
+            // увеличиваем стек
+            /*if (count == items.Length)
+                Resize(items.Length + 10);*/
+
+            items[count++] = item;
+        }
+        public S Pop()//поп :)
+        {
+            // если стек пуст, выбрасываем исключение
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+            S item = items[--count];
+           
+           /* if (count > 0 && count < items.Length - 10)
+                Resize(items.Length - 10);*/
+
+            return item;
+        }
+        public S Peek()//чтение верхнего элемента
+        {
+            return items[count - 1];
+        }
+
+        /*private void Resize(int max)//изменение максимальной длины
+        {
+            S[] tempItems = new S[max];
+            for (int i = 0; i < count; i++)
+                tempItems[i] = items[i];
+            items = tempItems;
+        }*/
+
+
+    }
+}
