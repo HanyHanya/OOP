554fd54c409f72984fc2c8be215b4be1bc42d13b
Yana <68700729+HanyHanya@users.noreply.github.com>
2020-11-23 19:37:33 +0300

    Задание 1

---
+++ lab9/LAB9/LAB9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LAB9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer Lang1 = new Programmer("C");
+            Programmer Lang2 = new Programmer("C", 5);
+            Lang1.Renamed += mes => Console.WriteLine(mes);
+            Lang1.Added += mes => Console.WriteLine(mes);
+            Lang2.Renamed += mes => Console.WriteLine(mes);
+            Lang2.Added += mes => Console.WriteLine(mes);
+            Lang1.Rename("C++");
+            Lang1.NewPr();
+
+            Lang2.Rename("C#");
+            Lang2.NewPr();
+
+        }
+    }
+}
---
+++ lab9/LAB9/LAB9/Programmer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB9
+{
+    public delegate void Language(string message);
+    public class Programmer
+    {
+        
+        public string name;
+        public int numOfPr = 0;
+        public Programmer() { }
+        public Programmer(string n) { name = n; }
+        public Programmer(string s, int n) { name = s; numOfPr = n; }
+        public event Language Renamed;
+        public event Language Added;
+        public void Rename(string str)
+        {
+            name = str;
+            Renamed?.Invoke($"Переименовано на {name}!");
+        }
+        public int NewPr()
+        {
+            numOfPr++;
+            Added?.Invoke($"Добавлено свойство! Их теперь {numOfPr}");
+            return numOfPr;
+        }
+    }
+
+}
