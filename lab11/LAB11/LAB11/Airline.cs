0739951db74f0373e8fe5b0e2afcc6067540f3c5
Yana <68700729+HanyHanya@users.noreply.github.com>
2020-12-08 11:07:29 +0300

    Лабораторная №11

---
+++ lab11/LAB11/LAB11/Airline.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB11
+{
+    public partial class Airline
+    {
+        private string destination;//пункт назначения
+        public uint numb;//номер рейса
+        public string type;//тип самолета
+        public int time;//время вылета
+        private string days;//дни недели
+        private const string airport = "Минск-1";
+        private readonly int planeID;
+        static int NumbOfPlanes = 0;
+
+        //конструкторы
+        public Airline(string des, string d, uint n, int t, string typ)//c параметрами
+        {
+            destination = des;
+            days = d;
+            numb = n;
+            time = t;
+            type = typ;
+            planeID = GetHashCode();
+            NumbOfPlanes++;
+        }
+        public Airline(int t, uint numb = 1)//с параметром по умолчанию
+        {
+            time = t;
+            planeID = GetHashCode();
+            NumbOfPlanes++;
+        }
+        private Airline()//без параметров, вызывать через методы
+        {
+            destination = "Неизвестно";
+            numb = 0;
+            time = 0;
+            days = "Неизвестно";
+            planeID = GetHashCode();
+            NumbOfPlanes++;
+        }
+        /*static Airline()//статический конструктор
+        {
+            type = "Пассажирский";
+        }*/
+
+        //свойства
+        public string Destination
+        {
+            get { return destination; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))//проверка
+                    destination = value;
+            }
+        }
+        public uint Numb
+        {
+            get
+            { return numb; }
+            set { numb = value; }
+        }
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public int Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+        public string Days
+        {
+            get { return days; }
+            private set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    days = value;
+            }//ограничен set
+        }
+
+
+        //методы
+        public static Airline addPlane()//способ вызова приватного конструктора
+        {
+            return new Airline();
+        }
+        /*public static void ShowInfo()
+        {
+            Console.WriteLine("Тип Самолетов: " + Airline.type);
+            Console.WriteLine($"Количество самолетов: {Airline.NumbOfPlanes}");
+        }*/
+    }
+    public partial class Airline
+    {
+        //переопределение
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(destination, numb, type, time, days);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Airline objectType)
+            {
+                if (this.destination == objectType.destination && this.numb == objectType.numb &&
+                    this.time == objectType.time && this.days == objectType.days)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            return $"Пункт назначения: {destination},\n Номер рейса: {numb},\n" +
+                   $"Тип самолета: {type},\n Врема вылета: {time},\n " +
+                   $"День вылета: {days}\n";
+        }
+    }
+
+    public class Person
+    {
+        public string _name;
+        public Person(string n)
+        {
+            _name = n;
+        }
+
+    }
+    public class Cat
+    {
+        public string _name;
+        public string _owner;
+        public Cat( string n, string o)
+        {
+            _name = n;
+            _owner = o;
+        }
+
+    }
+}
---
+++ lab11/LAB11/LAB11/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LAB11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            string[] month = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            int n;
+            Console.Write("Введите длину слова: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            var SelectedMonth = from m in month where m.Length == n select m;
+            Console.WriteLine($"Месяца длиной {n}:");
+            foreach (string s in SelectedMonth)
+                Console.WriteLine(s);
+            Console.WriteLine();
+
+            SelectedMonth = from m in month where m == "June" || m == "July" || m == "August" select m;
+            Console.WriteLine("Летние:");
+            foreach (string s in SelectedMonth)
+                Console.WriteLine(s);
+            Console.WriteLine();
+
+            SelectedMonth = from m in month where m == "December" || m == "January" || m == "February" select m;
+            Console.WriteLine("Зимние:");
+            foreach (string s in SelectedMonth)
+                Console.WriteLine(s);
+            Console.WriteLine();
+
+            SelectedMonth = from m in month orderby m select m;
+            Console.WriteLine("Месяца в алфавитном порядке:");
+            foreach (string s in SelectedMonth)
+                Console.WriteLine(s);
+            Console.WriteLine();
+
+            //Задание 2
+            List<Airline> Planes = new List<Airline>();
+            Airline Plane1 = new Airline("Минск", "Понедельник", 1, 1, "П");
+            Airline Plane2 = new Airline("Москва", "Вторник", 2, 2, "П");
+            Airline Plane3 = new Airline("Минск", "Среда", 3, 3, "П");
+            Airline Plane4 = new Airline("Москва", "Четверг", 4, 4, "П");
+            Airline Plane5 = new Airline("Минск", "Пятница", 5, 5, "Г");
+            Airline Plane6 = new Airline("Москва", "Суббота", 6, 6, "Г");
+            Airline Plane7 = new Airline("Москва", "Воскресенье", 7, 7, "Г");
+            Airline Plane8 = new Airline("Минск", "Понедельник", 8, 8, "Г");
+            Planes.Add(Plane1);
+            Planes.Add(Plane2);
+            Planes.Add(Plane3);
+            Planes.Add(Plane4);
+            Planes.Add(Plane5);
+            Planes.Add(Plane6);
+            Planes.Add(Plane7);
+            Planes.Add(Plane8);
+
+            //Задание 3
+            var SelectPlanes = from plane in Planes where plane.Destination == "Минск" select plane;
+            Console.WriteLine("Номера самолетов, летящих в Минск: ");
+            foreach (var p in SelectPlanes)
+                Console.Write(p.numb + " ");
+            Console.WriteLine();
+
+            SelectPlanes = from plane in Planes where plane.Days == "Понедельник" select plane;
+            Console.WriteLine("Номера самолетов, летящих в понедельник: ");
+            foreach (var p in SelectPlanes)
+                Console.Write(p.numb + " ");
+            Console.WriteLine();
+
+            SelectPlanes = from plane in Planes where plane.Days == "Понедельник" select plane;
+            Console.WriteLine("Номер самого позднего самолета, летящего в понедельник: ");
+            Console.WriteLine(SelectPlanes.Max(p => p.time));
+
+            SelectPlanes = from plane in Planes orderby plane.time select plane;
+            Console.WriteLine("Номера самолетов, отсортированные по времени вылета: ");
+            foreach (var p in SelectPlanes)
+                 Console.WriteLine("Номер: "+ p.numb + ". Время:" +p.time);
+            Console.WriteLine();
+
+            SelectPlanes = from plane in Planes where plane.type == "П" select plane;
+            int i = 0;
+            foreach (var p in SelectPlanes) i++;
+            Console.WriteLine($"Количество пассажирских самолетов: {i}.") ;
+            Console.WriteLine();
+
+            //Задание 4
+            Console.WriteLine("Все грузовые самолеты:");
+            var SPlanes = from p in Planes.Skip(2)
+                          where p.numb >= 3
+                          where p.type == "Г"
+                          orderby p.time descending
+                          select p;
+
+            foreach (var p in SPlanes)
+                Console.WriteLine(p);
+            Console.WriteLine();
+
+            Person p1 = new Person("Owner1");
+            Person p2 = new Person("Owner2");
+            Cat c1 = new Cat("Cat1", "Owner1");
+            Cat c2 = new Cat("Cat2", "Owner2");
+
+            List<Person> Persones = new List<Person>();
+            List<Cat> Cats = new List<Cat>();
+            Persones.Add(p1);
+            Persones.Add(p2);
+            Cats.Add(c1);
+            Cats.Add(c2);
+
+            var Result = from c in Cats
+                         join p in Persones on c._owner equals p._name
+                         select new { name = c._name, owner = p._name};
+            foreach ( var r in Result)
+                Console.WriteLine($"Имя питомца - {r.name}; имя хозяина - {r.owner}");
+            Console.WriteLine();
+        }
+    }
+}
